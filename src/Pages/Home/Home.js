777cd0a75290976cefd1e2ae9c910767174d7482
marioli99 <mazmariana.mo@gmail.com>
2022-04-25 23:41:24 -0300

    Estilizando Home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,20 +3,44 @@ import Carousel from "../../components/Carousel"
 import"../../components/DivModal"
 // import Header from "../Header";
 import CapitaoImg from "../../utils/img/Capitao.png" 
+import Coracao from "../../utils/img/coracao.png"
 import styled from "styled-components";
+
 
 const Visto = styled.div`
 display: flex;
+padding-left: 65px;
 `;
+const Destaque = styled.div`
+padding-left: 65px;
+margin-top:5%;
+h2{
+    font-size: 30px;
+    margin-bottom: 15px;
+}
 
+`;
 const Infos = styled.div`
-width: 30vw;
+margin-top: 30px;
+width: 36vw;
+padding-left: 60px;
+
  p {
      font-size: 14px;
      text-align: left;
+     padding-bottom: 5px;
+
  }
  h3{
      font-size: 16px;
+     padding-bottom: 5px;
+     }
+ h1{
+    padding-bottom: 5px;
+
+ }
+ img:hover{
+     cursor: pointer;
  }
 `;
 
@@ -27,14 +51,20 @@ export default class Home extends React.Component{
             {/* <Header/> */}
             <Visto>
             <><img src={CapitaoImg} alt=""/></>
-            <Infos><h3>Visto recentemente</h3>
+           
+            <Infos>
+            <img src={Coracao} alt=""/>    
+            <h3>Visto recentemente</h3>
             <h1>Capitao Fantástico</h1>
             <p>Nas florestas do estado de Washington, um pai cria seus seis filhos longe da civilização, em uma rígida rotina de aventuras. Ele é forçado a deixar o isolamento e leva sua família para encarar o mundo, desafiando sua ideia do que significa ser pai.</p></Infos>
             
             </Visto>
+            <Destaque>
+                <h2>Destaques</h2>
+                 <Carousel/>
+            </Destaque>
             
-            <h1>Destaques</h1>
-            <Carousel/>
+        
             
             </>
         )
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,39 +2,76 @@ import React from "react";
 import styled from "styled-components";
 import Carousel from "nuka-carousel";
 import Modal from "./DivModal";
+import Shrek from "../utils/img/Sherek.png";
+import Sozinho from "../utils/img/sozinho.png";
+import Miranha from "../utils/img/Miranha.png";
+import Sonho from "../utils/img/sonho.png";
+import QueHoras from "../utils/img/Quehoras.png"
 
-const Div = styled.div`
-  /* background-color: aquamarine; */
-  height: 40px;
-`;
 
-const Div2 = styled.div`
-  /* background-color: brown; */
-  height: 40px;
-`;
+
 
 const Button = styled.button`
- width: 40vw;
- height: 10vh;
- background-color: azure;
- color:black;
+ border: none;
+
+ img{
+   width: 21vw;
+ }
+ 
+`;
+const DivState = styled.div `
+  height: 45vh;
+
+  p{
+    font-size: 11px;
+  }
+
+  h3{
+    font-size: 15px;
+  }
 `;
 
 
 export default class MyCarousel extends React.Component {
     state= {
-        imagens : [
+        modalState : [
             {taAberto: false},
             {taAberto2: false},
         ],
   
-  
+    }
+
+    stateFilmes = {
+      filmes : [
+        {
+          Image:Shrek,
+          title:"Shrek",
+          text: "Para resgatar uma princesa das garras de um dragão que cospe fogo, o ogro Shrek se une a um companheiro improvável: um burro brincalhão."
+        },
+        {
+          Image:Sozinho,
+          title:"Hoje Eu Quero Voltar…",
+          text: "Um novo colega de sala transforma a vida do estudante Leonardo, que também é cego, e complica sua amizade com sua melhor amiga."
+        },
+        {
+          Image:Miranha,
+          title:"Spider-man",
+          text: "Após ser picado por uma aranha radioativa, o garoto Miles Morales logo aprende a lançar teias com seus parceiros de um universo alternativo."
+        },
+        {
+          Image:Sonho,
+          title:"Um Sonho de Liberdade",
+          text: "Condenado pelo assassinato da esposa e do amante dela, um banqueiro se apega à esperança e à amizade com um detento chamado Red para sobreviver à prisão."
+        },
+        {
+          Image:QueHoras,
+          title:"Que Horas Ela Volta?",
+          text: "Val é a fiel empregada domestica de uma família rica. Mas a chegada de sua filha gera tensão na casa e faz com que ela comece a questionar esse papel."
+        },
+      ]
     }
 
     handle = () => {
-      this.setState({ taAberto: !this.state.taAberto });
-    };
-    handle2 = () => {
       this.setState({ taAberto2: !this.state.taAberto2 });
     };
   render() {
@@ -44,20 +81,28 @@ export default class MyCarousel extends React.Component {
         autoplay={true}
         speed={3000}
         wrapAround={true}
-        slidesToShow={1}
+        slidesToShow={4.5}
         defaultControlsConfig={{
-          nextButtonText: ">",
+          nextButtonText:">",
           prevButtonText: "<",
           pagingDotsStyle: {
             fill: "none",
-          },
+          }
         }}
+        cellSpacing={10}
+        adaptiveHeight={true}
       >
+
+        {/* <Button onClick={this.handle}><img src={item.Image} alt=""/></Button> */}
+        {this.stateFilmes.filmes.map((item)=>(
+          <DivState>
+           <Button onClick={this.handle}><img src={item.Image} alt=""/></Button>
+           <h3>{item.title}</h3>
+           <p>{item.text}</p>
+
+          </DivState>
+        ))}
         
-        <Div> <Button onClick={this.handle}>Text Modal</Button></Div>
-        <Div2> <Button onClick={this.handle2}>Text Modal</Button></Div2>
-        
-       
       </Carousel>
         {this.state.taAberto && <Modal />}
         {this.state.taAberto2 && <Modal />}
